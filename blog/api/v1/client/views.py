2b4d5cd0d8dba37f7930fd6842540c1e48e661fa
shtalina <99807081+shtalina@users.noreply.github.com>
2023-11-30 02:42:54 +0700

    Update views.py

--- blog/api/v1/client/views.py
+++ blog/api/v1/client/views.py
@@ -1,6 +1,6 @@
-from api.v1.client.serializers.blog_serializer import UserSerializer, ArticleSerializer, CommentSerializer, CategorySerializer
+from api.v1.client.serializers.blog_serializer import UserSerializer, ArticleSerializer, CommentsSerializer, CategorySerializer, TagsSerializer
 from rest_framework import viewsets
-from main.models import User, Article, Comment, Category
+from main.models import User, Article, Comments, Category, Tags
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,10 +11,14 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
+class CommentsViewSet(viewsets.ModelViewSet):
+    queryset = Comments.objects.all()
     serializer_class = CommentSerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class TagsViewSet(viewsets.ModelViewSet):
+    queryset = Tags.objects.all()
+    serializer_class = TagsSerializer
