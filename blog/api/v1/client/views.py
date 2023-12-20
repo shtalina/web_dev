68786ff062223ef22a2f5c69168affacf45b6b5a
shtalina <99807081+shtalina@users.noreply.github.com>
2023-12-21 00:17:16 +0700

    Update views.py

--- blog/api/v1/client/views.py
+++ blog/api/v1/client/views.py
@@ -8,94 +8,31 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class ArticleViewSet(viewsets.ModelViewSet):
+
+class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     filterset_fields = ['category', 'published']
 
+
 class ArticleOnlyPublishedViewSet(
         mixins.RetrieveModelMixin,
         mixins.ListModelMixin,
-        viewsets.GenericViewSet
+        viewsets.GenericViewSet,
     ):
-
-    queryset = Article.objects.filter(published=True)
+    queryset = Article.objects.filter(published=True).all()
     serializer_class = ArticleSerializer
 
-class CommentsViewSet(viewsets.ModelViewSet):
+
+class CommentsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Comments.objects.all()
     serializer_class = CommentsSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        article_id = self.request.query_params.get('article', None)
-        if article_id is not None:
-            return Comments.objects.filter(article=article_id)
-        else:
-            return Comments.objects.all()
-
-    def get_permissions(self):
-        if self.action == 'create':
-            permission_classes = [permissions.IsAuthenticated]
-        elif self.action in ['update', 'partial_update', 'destroy']:
-            permission_classes = [permissions.IsAuthenticated]
-
-        return [permission() for permission in permission_classes]
-
-    def create(self, request):
-        if not request.user.is_authenticated:
-            return Response({"message": "Comment created successfully"}, status=status.HTTP_201_CREATED)
-        # Ваша логика создания комментария
-        return Response({"message": "You need to be authenticated to create a comment"},
-                            status=status.HTTP_401_UNAUTHORIZED)
-
-    def update(self, request, pk=None):
-        if request.user.is_authenticated:
-            return Response({"message": "Comment updated successfully"})
-        # Ваша логика обновления комментария
-        return Response({"message": "You need to be authenticated to update a comment"}, status=status.HTTP_401_UNAUTHORIZED)
 
 
-    def destroy(self, request, pk=None):
-        if not request.user.is_authenticated:
-            return Response({"message": "Comment deleted successfully"})
-        # Ваша логика удаления комментария
-        return Response({"message": "You need to be authenticated to delete a comment"},
-                            status=status.HTTP_401_UNAUTHORIZED)
-
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    def get_permissions(self):
-        if self.action == 'create':
-            permission_classes = [permissions.IsAuthenticated]
-        elif self.action in ['update', 'partial_update', 'destroy']:
-            permission_classes = [permissions.IsAuthenticated]
-        else:
-            permission_classes = [permissions.AllowAny]
-        return [permission() for permission in permission_classes]
-
-    def create(self, request):
-        if not request.user.is_authenticated:
-            return Response({"message": "Category created successfully"}, status=status.HTTP_201_CREATED)
-        # Ваша логика создания комментария
-        return Response({"message": "You need to be authenticated to create a category"},
-                            status=status.HTTP_401_UNAUTHORIZED)
-
-    def update(self, request, pk=None):
-        if request.user.is_authenticated:
-            return Response({"message": "Categoryt updated successfully"})
-        # Ваша логика обновления комментария
-        return Response({"message": "You need to be authenticated to update a Category"}, status=status.HTTP_401_UNAUTHORIZED)
-
-
-    def destroy(self, request, pk=None):
-        if not request.user.is_authenticated:
-            return Response({"message": "Comment deleted successfully"})
-        # Ваша логика удаления комментария
-        return Response({"message": "You need to be authenticated to delete a comment"},
-                            status=status.HTTP_401_UNAUTHORIZED)
 
 class ArticleManageViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
@@ -104,3 +41,18 @@ class ArticleManageViewSet(viewsets.ModelViewSet):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
+
+class CategoryManageViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filterset_fields = ['article']
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CommentsManageViewSet(viewsets.ModelViewSet):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+    filterset_fields = ['article']
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
