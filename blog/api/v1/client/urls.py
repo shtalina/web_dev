f8caaff74ea226d45efb80bffecba1aed0621608
shtalina <99807081+shtalina@users.noreply.github.com>
2023-12-21 00:17:37 +0700

    Update urls.py

--- blog/api/v1/client/urls.py
+++ blog/api/v1/client/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
-from api.v1.client.views import UserViewSet, ArticleViewSet, ArticleManageViewSet, CommentsViewSet, CategoryViewSet, ArticleOnlyPublishedViewSet
+from api.v1.client.views import (UserViewSet, ArticleViewSet,
+                                 ArticleManageViewSet, CommentsViewSet, CategoryViewSet,
+                                 ArticleOnlyPublishedViewSet, CommentsManageViewSet, CategoryManageViewSet)
 
 
 router = routers.DefaultRouter()
@@ -11,7 +13,9 @@ router.register(r'articles', ArticleViewSet, basename="articles")
 router.register(r'articles_manage', ArticleManageViewSet)
 router.register(r'articles_only_published', ArticleOnlyPublishedViewSet, basename="articles")
 router.register(r'comments', CommentsViewSet, basename='comment')
+router.register(r'comments_manage', CommentsManageViewSet)
 router.register(r'categories', CategoryViewSet)
+router.register(r'categories_manage', CategoryManageViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
